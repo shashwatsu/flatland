bad7d8fe812c30469b783664666b53626a8da751
shashwatsu <shashwatsuthar@outlook.com>
2022-08-13 20:56:13 +0530

    First Commit

---
+++ Flatland.py
@@ -0,0 +1,42 @@
+import random
+import numpy
+from PIL import Image
+
+World = numpy.zeros((500, 500, 3), dtype = numpy.uint8)
+
+def GenerateDirt():
+    Prev = 200
+    State = 0
+    Limit = None
+
+    for i in range(500):
+        if State == 0:
+            if Limit == None:
+                Limit = Prev-random.randint(4, 12)
+            
+            elif Prev < Limit:
+                State = 1
+                Limit = None
+            
+            else:
+                Prev = Prev-random.randint(0,3)
+                World[Prev:499, i] = 255
+                print(i)
+            
+        elif State == 1:
+            if Limit == None:
+                Limit = Prev+random.randint(4,12)
+
+            elif Prev > Limit:
+                State = 0
+                Limit = None
+
+            else:
+                Prev = Prev+random.randint(0,3)
+                World[Prev:499, i] = 255
+                print(i)
+
+World[0:499, 0:499] = 50
+GenerateDirt()
+Render = Image.fromarray(World)
+Render.save("Flatland.png")
