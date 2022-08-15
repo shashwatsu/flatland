7b0e6a20ff0795e0f87906c79efd494690b453b1
shashwatsu <shashwatsuthar@outlook.com>
2022-08-15 17:54:01 +0530

    Added Alpha Channel

--- Flatland.py
+++ Flatland.py
@@ -2,7 +2,7 @@ import random
 import numpy
 from PIL import Image
 
-World = numpy.zeros((500, 500, 3), dtype = numpy.uint8)
+World = numpy.zeros((500, 500, 4), dtype = numpy.uint8)
 
 def GenerateDirt():
     global Frame
@@ -10,7 +10,7 @@ def GenerateDirt():
     Prev = 400
     State = 0
     Limit = None
-    DirtColor = [84, 46, 22]
+    DirtColor = [84, 46, 22, 255]
     for i in range(500):
         if State == 0:
             if Limit == None:
@@ -51,21 +51,28 @@ def GenerateDirt():
                 Frame.append(Prev)
 
 def GenerateGrass():
-    Grass = [54, 115, 49]
+    Grass = [54, 115, 49, 255]
     for i in range(499):
         World[Frame[i]:Frame[i]+random.choice((4,4,4,3,3)), i] = Grass
 
 def GenerateStone():
-    Stone = [153, 153, 153]
+    Stone = [153, 153, 153, 255]
     for i in range(499):
         for j in range(499-Frame[i]):
             if random.randint(1,5000)>4998:
                 World[(499-j):(499-j)+3, i:i+3] = Stone
 
-SkyColor = [188, 220, 245]
+def GenerateTree():
+    TreeImage = Image.open("assets/Tree.png")
+    TreeImage = TreeImage.resize((70,70), 0)
+    TreeArray = numpy.asarray(TreeImage, dtype=numpy.uint8)
+    print(numpy.shape(TreeArray))
+
+SkyColor = [188, 220, 245, 255]
 World[0:499, 0:499] = SkyColor
 GenerateDirt()
 GenerateGrass()
 GenerateStone()
+GenerateTree()
 Render = Image.fromarray(World)
-Render.save("Flatland.jpg", format='JPEG', quality='web_maximum', subsampling=0,quantization=0)
+Render.save("Flatland.png", format='png', quality='web_maximum', subsampling=0,quantization=0)
